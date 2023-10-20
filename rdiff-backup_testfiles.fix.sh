79caae649233e375610aaf0203d788b7bd72e842
Eric L. <ewl+pine@lavar.de>
2023-10-20 07:33:46 +0200

    Make fix of testfiles verbose
    This allows to see if something was really changed in Ansible

--- rdiff-backup_testfiles.fix.sh
+++ rdiff-backup_testfiles.fix.sh
@@ -1,4 +1,4 @@
-find rdiff-backup_testfiles -user 1000 \! -type l -print0 | xargs -0 chown ${1:-$SUDO_UID}
-find rdiff-backup_testfiles -group 1000 \! -type l -print0 | xargs -0 chgrp ${2:-$SUDO_GID}
-find rdiff-backup_testfiles -user 1000 -type l -print0 | xargs -0 chown -h ${1:-$SUDO_UID}
-find rdiff-backup_testfiles -group 1000 -type l -print0 | xargs -0 chgrp -h ${2:-$SUDO_GID}
+find rdiff-backup_testfiles -user 1000 \! -type l -print0 | xargs -0 chown -v ${1:-$SUDO_UID}
+find rdiff-backup_testfiles -group 1000 \! -type l -print0 | xargs -0 chgrp -v ${2:-$SUDO_GID}
+find rdiff-backup_testfiles -user 1000 -type l -print0 | xargs -0 chown -vh ${1:-$SUDO_UID}
+find rdiff-backup_testfiles -group 1000 -type l -print0 | xargs -0 chgrp -vh ${2:-$SUDO_GID}
